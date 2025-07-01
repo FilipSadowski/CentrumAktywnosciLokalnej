1d41aba1bf9df821ccb664c01fb1c3dd46ee4ecd
FilipSadowski <39171579+FilipSadowski@users.noreply.github.com>
2025-07-02 01:36:40 +0200

    Update GallerySection.tsx

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -5,17 +5,17 @@ import { motion, AnimatePresence } from 'framer-motion'
 import { Info } from 'lucide-react'
 
 const images = [
-  { src: '/images/1.jpg', title: 'Plansza 1 - Orientacja i analizy', description: 'Kolorowe oświetlenie podczas wydarzenia z zamkniętą seceną.' },
-  { src: '/images/2.jpg', title: 'Plansza 2 - Zagospodarowanie terenu', description: 'Koncert!!! Poszerzenie widowni o wyspę słodową.' },
-  { src: '/images/3.jpg', title: 'Plansza 3 - Strefy funkcjonalne', description: 'Ciepłe oświetlenie wpasowuje się w przyjemne otoczenie.' },
-  { src: '/images/4.jpg', title: 'Plansza 4 - Budynek A', description: 'Nowoczesne kszatłty pomagające kształtować akustykę i umozliwiające zmienianie pomieszczenia.' },
-  { src: '/images/5.jpg', title: 'Plansza 5 - Budynek B', description: 'Główne i wejsciowe foyer, restauracja, sala uzupełnione o obsługę.' },
-  { src: '/images/6.jpg', title: 'Plansza 6 - Budynek C', description: 'Antresola foyer, balkony i biura znajdują się na tej kondygnacji.' },
-  { src: '/images/7.jpg', title: 'Połączone plansze', description: 'Niewielki garaż z widną samochodową.' },
-  { src: '/images/8.jpg', title: 'Aksonometria', description: 'Serce budynku w środku.' },
-  { src: '/images/9.jpg', title: 'Wizualizacja najścia widokowego', description: 'Serce budynku w środku.' },
-  { src: '/images/10.jpg', title: 'Wizualizacja dziedzińca', description: 'Serce budynku w środku.' },
-  { src: '/images/11.jpg', title: 'Wizualizacja elewacji', description: 'Serce budynku w środku.' },
+  { src: '/images/1.jpg', title: 'Plansza 1 - Orientacja i analizy', description: 'Analizy i wnioski z analiz. Graficzne przedstawienie założeń projektowych. Lokalizacja założenia. Orientacja działki w skali 1:2000.' },
+  { src: '/images/2.jpg', title: 'Plansza 2 - Zagospodarowanie terenu', description: 'PZT w skali 1:500, aksonometria zespołu budynków, wizualizacja.' },
+  { src: '/images/3.jpg', title: 'Plansza 3 - Strefy funkcjonalne', description: 'Szkic zagospodarowania terenu pokazujący zaprojektowany teren i powiązania z wnętrzem budynku w skali 1:250, schematy, zestawienia powierzchni, wizualizacja zagospodarowania terenu wzdłuż zewnętrznej elewacji budynku C.' },
+  { src: '/images/4.jpg', title: 'Plansza 4 - Budynek A', description: 'Rzut parteru w skali 1:100, przekrój podłużny w skali 1:100, rzut tarasów w skali 1:200, rzut dachu w skali 1:200, elewacje północna i południowa w skali 1:200, zestawienie pomieszczeń budynku A, wizualizacja placu wewnętrznego na tle wejścia między budynkami A i B.' },
+  { src: '/images/5.jpg', title: 'Plansza 5 - Budynek B', description: 'Rzut parteru w skali 1:100, przekrój podłużny w skali 1:100, przekrój poprzeczny w skali 1:100, rzut tarasów w skali 1:200, rzut dachu w skali 1:200, elewacje wschodnia i zachodnia w skali 1:200, schematy i spis przegród, detal architektoniczny elewacji od wewnątrz dziedzińca w skali 1:20, wizualizacja placu wewnętrznego  fragmentem elewacji wewnętrznej budynku C, ze skupieniem na kręcone schody na tle elewacji wewnętrznej budynku B.' },
+  { src: '/images/6.jpg', title: 'Plansza 6 - Budynek C', description: 'Rzut parteru w skali 1:100, przekrój podłużny w skali 1:100, przekrój poprzeczny w skali 1:100, rzut tarasów w skali 1:200, rzut dachu w skali 1:200, elewacje północna i południowa w skali 1:200, zestawienie pomieszczeń budynków A, B i C, detal architektoniczny elewacji od zewnątrz założenia w skali 1:20,przedstawienie materiałów użytych na elewacjach, fragment widoku elewacji frontowej w skali 1:200.' },
+  { src: '/images/7.jpg', title: 'Połączone plansze', description: 'Część graficzna została podana na 6 planszach formatu A1 w układzie pionowym. Są one zaprojektowane tak, aby tworzyły spójną całość ustawione obok siebie w odpowiedniej kolejności i zarazem miały sens kompozycyjny i merytoryczny w przypadku oglądania ich pojedynczo.' },
+  { src: '/images/8.jpg', title: 'Aksonometria', description: 'Prezentacja zespołu budyków w widoku z góry - forma charakterystyczna dla architektów na studiach. W rzeczywistości nikt nie widzi budynku w taki sposób, a nawet przy chęciach zaprezentowania go z góry powinno się stosować perspektywę, aby nie zniekształcać proporcji. Aksonometria służy więc bardziej jak schemat rozwiązań, niż rzeczywiste pokazywanie wyglądu budynku w przestrzeni.' },
+  { src: '/images/9.jpg', title: 'Wizualizacja najścia widokowego', description: 'Ważny widok w koncepcji zespołu budynków.' },
+  { src: '/images/10.jpg', title: 'Wizualizacja dziedzińca', description: 'Serce założenia. Charakterystyczny akcent w kompozycyjnym centrum przykuwa uwagę. Sale skierowane "twarzą" do środka, przecinające się ciągi piesze oraz miejsca do spędzania wolnego czasu - tętnią życiem.' },
+  { src: '/images/11.jpg', title: 'Wizualizacja elewacji', description: 'Przejście wzdłuż zewnętrznej elewacji zespołu. Podczas gdy po drugiej stronie budynku - na dziedzieńcu - toczy się zycie, deptak ten pozostaje spokojny. Zaprojektowany z myślą zapewnienia poczucia komfortu i bezpieczeństwa w tym pośrednim kontakcie z projektowaną przestrzenią.' },
 ]
 
 export default function GallerySection({ setFullscreen }: { setFullscreen: (v: boolean) => void }) {
